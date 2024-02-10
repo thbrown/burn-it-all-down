d0ed43204b79d431207dc279dc339872488aebcf
Thomas Brown <thomasbrown@palantir.com>
2024-02-09 23:25:53 -0700

    Make tiles re-sizable. Add small delay on victory. Use bloomfilter for solvable algo. Don't crash is color string isn't long enough

--- index.js
+++ index.js
@@ -2,36 +2,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const gameContainer = document.getElementById('game-container');
     const winMessage = document.getElementById('win-message');
     const colors = ['red', 'blue', 'green'];
-    const squareSize = 48; // The size of each square
     const queryParams = new URLSearchParams(window.location.search);
-    console.log("Params", queryParams.get('width'), queryParams.get('height'), queryParams.get('colors'));
+    console.log("Params", queryParams.get('width'), queryParams.get('height'), queryParams.get('colors'), queryParams.get('size'));
     let gameWon = false;
-
-    // Set the innerHTML to the appropriate SVG based on the color
-    const RED_SVG = `
-        <svg class="red-filter" width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <g id="Artboard-Copy" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                <path d="M11.6223011,0 C11.6223011,1.70940171 12.1128933,3.07692308 13.0940778,4.1025641 C16.3646926,6.4957265 18,9.23076923 18,12.3076923 C18,15.7264957 16.0376311,18.2905983 12.1128933,20 C15,17 14.5658545,15.7692308 11.6223011,12 C8.5,13.5 9,14.5 9.5,16.5 C8.45242566,16.5 7.5,16.5 7,16 C7,16.6837607 8.19671588,18.5 8.95242566,20 C5.02768789,18.974359 0.829272269,12.8205128 2.30104893,12.3076923 C3.28223337,11.965812 4.42694856,12.1367521 5.73519448,12.8205128 C4.09988708,6.66666667 6.06225596,2.39316239 11.6223011,0 Z" id="Path-3" fill="#000000"></path>
-            </g>
-        </svg>`;
-    const BLUE_SVG = `
-        <svg class="blue-filter" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-        width="20px" height="20px" viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve">
-        <g id="tint">
-            <g>
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M9.86,2c0,0-5.88,7.18-5.88,10.17c0.01,3.23,2.8,5.83,5.98,5.83
-                    c3.18-0.01,6.04-2.63,6.03-5.86C15.99,9.05,9.86,2,9.86,2z"/>
-            </g>
-        </g>
-        </svg>`;
-    const GREEN_SVG = `
-        <svg class="green-filter" width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                <g id="Tree" transform="translate(2.000000, 0.000000)" fill="#000000" fill-rule="nonzero">
-                    <path d="M9,15.5416666 L9,20 L7,20 L7,15.5416666 L0,17 L4.5,11.375 L2,12 L5.65517241,6.51724138 L4,7 L8,0 L12,7 L10.3448276,6.51724138 L14,12 L11.5,11.375 L16,17 L9,15.5416666 Z"></path>
-                </g>
-            </g>
-        </svg>`;
 
     const colorMap = {
         'red': 0, // Binary: 00
@@ -59,7 +32,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const encodeGridStateToBase64 = () => {
         const squares = document.querySelectorAll('.square');
         const gridState = Array.from(squares).map(square => square.style.backgroundColor);
-        //console.log("Grid State", gridState);
         const binaryString = encodeToBinary(gridState);
         return binaryToUrlSafeBase64(binaryString);
     };
@@ -149,14 +121,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         
         //const colorState = encodeGridStateToBase64();
         //console.log("Checking game", decodeBase64GridState(colorState, squaresPerColumn, squaresPerRow), isGameWinnable(colorState, squaresPerColumn, squaresPerRow));
-        /*if (!isGameWinnable(colorState, squaresPerColumn, squaresPerRow)) {
-            console.log("Not Winnable");
-            gameContainer.innerHTML = '';
-            initGameGrid();
-        } else {
-            console.log("Winnable")
-        }
-        */
+        //while (!isGameWinnable(colorState, squaresPerColumn, squaresPerRow)) {
+        //    console.log("Not Winnable");
+        //    gameContainer.innerHTML = '';
+        //    initGameGrid();
+        //} else {
+        //    console.log("Winnable")
+        //}
+        
     };
 
     // Helper function to get the adjacent indices for a given index
@@ -186,11 +158,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
         return adjInd;
     };
 
+    const getSquareSize = (rows, cols) => {
+        if(queryParams.get('size') === "auto") {
+            return Math.min(Math.floor(window.innerWidth / rows), Math.floor(window.innerHeight / cols));
+        } else {
+            return queryParams.get('size') ?? 48;
+        }
+    }
+
+    const decodeWithErrorChecks = (base64State, rows, cols) => {
+        try {
+        return decodeBase64GridState(queryParams.get('colors'), squaresPerColumn, squaresPerRow)
+        } catch (e) {
+            console.warn("Error decoding", base64State, rows, cols, "using random colors instead.");
+            return null;
+        }
+    }
+
     const isGameWinnable = (base64State, rows, cols) => {
         const gridState = decodeBase64GridState(base64State, rows, cols);
         console.log("Checking game", gridState);
     
-        const visitedStates = new Set(); // To track visited states
+        //const visitedStates = new Set(); // To track visited states
+        var visitedStates = new BloomFilter(
+            32 * 99999999, // number of bits to allocate.
+            16        // number of hash functions.
+          );
+          
         const stack = []; // Stack for iterative exploration
     
         // Start with the initial state and index 0
@@ -212,7 +206,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     
             // Convert the current state to a string to check if it's been visited
             const stateString = currentState.join('') + "-" + index;
-            if (visitedStates.has(stateString)) {
+            if (visitedStates.test(stateString)) {
                 continue; // This state has already been visited
             }
             visitedStates.add(stateString); // Mark this state as visited
@@ -238,9 +232,37 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Calculate the number of squares that fit per row/column
     let squaresPerRow = queryParams.get('width') ?? Math.floor(window.innerWidth / squareSize);
     let squaresPerColumn = queryParams.get('height') ?? Math.floor(window.innerHeight / squareSize);
-    let decodedColors = queryParams.get('colors') ? decodeBase64GridState(queryParams.get('colors'), squaresPerColumn, squaresPerRow) : null;
+    let squareSize = getSquareSize(squaresPerRow, squaresPerColumn);
+    let decodedColors = queryParams.get('colors') ? decodeWithErrorChecks(queryParams.get('colors'), squaresPerColumn, squaresPerRow) : null;
     let totalSquares = squaresPerRow * squaresPerColumn;
     console.log("Number", squaresPerRow, squaresPerColumn, decodedColors);
+
+    // 48/? = 20
+        // Set the innerHTML to the appropriate SVG based on the color
+    const RED_SVG = `
+        <svg class="red-filter" width="${Math.floor(squareSize/(48/20))}px" height="${Math.floor(squareSize/(48/20))}px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Artboard-Copy" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <path d="M11.6223011,0 C11.6223011,1.70940171 12.1128933,3.07692308 13.0940778,4.1025641 C16.3646926,6.4957265 18,9.23076923 18,12.3076923 C18,15.7264957 16.0376311,18.2905983 12.1128933,20 C15,17 14.5658545,15.7692308 11.6223011,12 C8.5,13.5 9,14.5 9.5,16.5 C8.45242566,16.5 7.5,16.5 7,16 C7,16.6837607 8.19671588,18.5 8.95242566,20 C5.02768789,18.974359 0.829272269,12.8205128 2.30104893,12.3076923 C3.28223337,11.965812 4.42694856,12.1367521 5.73519448,12.8205128 C4.09988708,6.66666667 6.06225596,2.39316239 11.6223011,0 Z" id="Path-3" fill="#000000"></path>
+            </g>
+        </svg>`;
+    const BLUE_SVG = `
+        <svg class="blue-filter" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        width="${Math.floor(squareSize/(48/20))}px" height="${Math.floor(squareSize/(48/20))}px" viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve">
+        <g id="tint">
+            <g>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M9.86,2c0,0-5.88,7.18-5.88,10.17c0.01,3.23,2.8,5.83,5.98,5.83
+                    c3.18-0.01,6.04-2.63,6.03-5.86C15.99,9.05,9.86,2,9.86,2z"/>
+            </g>
+        </g>
+        </svg>`;
+    const GREEN_SVG = `
+        <svg class="green-filter" width="${Math.floor(squareSize/(48/20))}px" height="${Math.floor(squareSize/(48/20))}px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="Tree" transform="translate(2.000000, 0.000000)" fill="#000000" fill-rule="nonzero">
+                    <path d="M9,15.5416666 L9,20 L7,20 L7,15.5416666 L0,17 L4.5,11.375 L2,12 L5.65517241,6.51724138 L4,7 L8,0 L12,7 L10.3448276,6.51724138 L14,12 L11.5,11.375 L16,17 L9,15.5416666 Z"></path>
+                </g>
+            </g>
+        </svg>`;
 
     // Function to change color of a square
     const changeColor = (square) => {
@@ -300,7 +322,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 height: squaresPerColumn,
                 colors: colorState
             }
-            window.history.pushState(JSON.stringify(state), '', `${document.location.pathname}?colors=${colorState}&width=${squaresPerRow}&height=${squaresPerColumn}`);
+            window.history.pushState(JSON.stringify(state), '', `${document.location.pathname}?colors=${colorState}&width=${squaresPerRow}&height=${squaresPerColumn}&size=${queryParams.get('size') ?? 64}`);
             console.log("Modifying url", colorState, squaresPerRow, squaresPerColumn);
         } catch (e) {
             console.warn("Problem pushing state", e);
@@ -310,14 +332,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('replay').addEventListener('click', replay);
 
     window.addEventListener("popstate", (e) => {
-        console.log("Popstate raw", e.state);
         let prevState = null;
         try {
             prevState = JSON.parse(e.state);
         } catch (e) {
             prevState = {};
         }
-        console.log("Popstate", prevState);
         squaresPerRow = prevState.width ?? Math.floor(window.innerWidth / squareSize);
         squaresPerColumn = prevState.height ?? Math.floor(window.innerHeight / squareSize);
         decodedColors = prevState.colors ? decodeBase64GridState(prevState.colors, squaresPerColumn, squaresPerRow) : null;
@@ -361,8 +381,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         // Check for win after each click
         if (checkWin()) {
-            showWinModal();
             gameWon = true;
+            setTimeout(() => {
+                showWinModal();
+            }, 500);
         }
     });
 
